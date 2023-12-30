0913d929a498e9eabc31b5546d6c07b1df4bc265
James Sinclair <sinclair.j@gmail.com>
2023-12-30 21:50:06 +1100

    Day 8 pt 1 solution

--- day-08/day_08.py
+++ day-08/day_08.py
@@ -1,10 +1,35 @@
 import re
 
-        
 
 def part_1(lines):
-    total = 0
-    return total
+    # parse input
+    # first line gives directions
+    directions = lines[0].strip()
+
+    # use a dict to store the map for easy lookup
+    map = {}
+    position = 'AAA'
+    end = 'ZZZ'
+    i = 0
+    
+    # 3rd line onwards defines map
+    for line in lines[2:]:
+        match = re.match(r'(\w+) = \((\w+), (\w+)\)', line)
+        source, left, right = match.groups()
+        map[source] = (left, right)
+
+    # follow the directions to get to the destination
+    while position != end:
+        # move to the next position - wrap around with directions
+        direction = directions[i % len(directions)]
+        if direction == 'L':
+            position = map[position][0]
+        elif direction == 'R':
+            position = map[position][1]
+        
+        i += 1
+    
+    return i
     
 def part_2(lines):
     total = 0
@@ -15,12 +40,18 @@ if __name__ == '__main__':
     with open('test.txt', 'r') as f:
         test_lines = f.readlines()
 
+    with open('test_2.txt', 'r') as f:
+        test_2_lines = f.readlines()
+
     with open('input.txt', 'r') as f:
         input_lines = f.readlines()
     
     print("Part 1 ======================")
     test_vals = part_1(test_lines)
     print(f"Test output: {test_vals}")
+    test_2_vals = part_1(test_2_lines)
+    print(f"Test 2 output: {test_2_vals}")
+    
     input_vals = part_1(input_lines)
     print(f"Real output: {input_vals}")
 
