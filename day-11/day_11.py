fc4405852885abcfa0c9c71ca80ea22c91f6e603
James Sinclair <sinclair.j@gmail.com>
2024-01-04 23:31:59 +1100

    Day 11 pt 1 solution

--- day-11/day_11.py
+++ day-11/day_11.py
@@ -1,10 +1,55 @@
 import re
 
         
+def print_grid(grid):
+    g = [''.join(row) for row in grid]
+    print('\n'.join(g))
 
 def part_1(lines):
-    total = 0
-    return total
+    # parse input
+    data = []
+    for line in lines:
+        new_data = list(line.strip())
+        data.append(new_data)
+        # expand space along rows
+        if all([d == '.' for d in new_data]):
+            data.append(new_data)
+    
+    # expand space along columns
+    data = list(zip(*data))
+    new_data = []
+    for i, row in enumerate(data):
+        new_data.append(row)
+        if all([d == '.' for d in row]):
+            new_data.append(row)
+    
+    # transpose back to row/column format
+    data = list(zip(*new_data))
+    print(data)
+    print_grid(data)
+
+    # extract galaxy positions
+    galaxy_positions = []
+    for i, row in enumerate(data):
+        for j, _ in enumerate(row):
+            if row[j] == '#':
+                galaxy_positions.append((i, j))
+        
+    print(f"Num galaxies: {len(galaxy_positions)}")
+
+    # compute pairwise distances (manhattan distance)
+    distances = []
+    for i, pos1 in enumerate(galaxy_positions):
+        for j, pos2 in enumerate(galaxy_positions):
+            # skip if same position or if we've already computed this distance
+            if i == j or i > j:
+                continue
+            dist = abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
+            distances.append(dist)
+
+    # return sum of distances
+    print(f"Distances: {distances}")
+    return sum(distances)
     
 def part_2(lines):
     total = 0
@@ -24,8 +69,8 @@ if __name__ == '__main__':
     input_vals = part_1(input_lines)
     print(f"Real output: {input_vals}")
 
-    print("Part 2 ======================")
-    test_vals = part_2(test_lines)
-    print(f"Test output: {test_vals}")
-    input_vals = part_2(input_lines)
-    print(f"Real output: {input_vals}")
+    #print("Part 2 ======================")
+    #test_vals = part_2(test_lines)
+    #print(f"Test output: {test_vals}")
+    #input_vals = part_2(input_lines)
+    #print(f"Real output: {input_vals}")
