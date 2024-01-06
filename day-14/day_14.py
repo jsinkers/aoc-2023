f494811075756d65378000f20a183edcff29aa3e
James Sinclair <sinclair.j@gmail.com>
2024-01-06 20:24:40 +1100

    Day 14 pt 1 solution

--- day-14/day_14.py
+++ day-14/day_14.py
@@ -1,10 +1,48 @@
 import re
-
         
+def print_grid(grid):
+    grid = list(zip(*grid))
+    g = [''.join(row) for row in grid]
+    print('\n'.join(g))
 
 def part_1(lines):
-    total = 0
-    return total
+    # parse input
+    rock_positions = []
+    for line in lines:
+        line = line.strip()
+        row = line
+        rock_positions.append(row)
+        
+    # transpose
+    rock_positions = list(zip(*rock_positions))
+    #print_grid(rock_positions)
+
+    # split by # 
+    rock_positions = [''.join(row).split('#') for row in rock_positions]
+    #print(rock_positions)
+
+    # move all O rocks to start of list
+    rotated_rock_positions = []
+    for row in rock_positions:
+        new_row = []
+        for rock_group in row:
+            len_rock_group = len(rock_group)
+            num_rocks = rock_group.count('O')
+            new_row.append('O'*num_rocks + '.'*(len_rock_group - num_rocks))
+
+        #print(new_row)
+        new_row = '#'.join(new_row)
+        #print(new_row)
+        rotated_rock_positions.append(new_row)
+        #print(rotated_rock_positions)
+    
+    #print_grid(rotated_rock_positions)
+    # calculate load for each rock
+    # transpose again and reverse
+    rotated_rock_positions = reversed(list(zip(*rotated_rock_positions)))
+    score = [(i+1, row.count('O')) for i, row in enumerate(rotated_rock_positions)]
+    score = sum([i*count for i, count in score])
+    return score
     
 def part_2(lines):
     total = 0
@@ -24,8 +62,8 @@ if __name__ == '__main__':
     input_vals = part_1(input_lines)
     print(f"Real output: {input_vals}")
 
-    print("Part 2 ======================")
-    test_vals = part_2(test_lines)
-    print(f"Test output: {test_vals}")
-    input_vals = part_2(input_lines)
-    print(f"Real output: {input_vals}")
+    # print("Part 2 ======================")
+    # test_vals = part_2(test_lines)
+    # print(f"Test output: {test_vals}")
+    # input_vals = part_2(input_lines)
+    # print(f"Real output: {input_vals}")
