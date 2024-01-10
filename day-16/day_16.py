b3112521341cfe184fa676c3ba6d663c6bd1a08d
James Sinclair <sinclair.j@gmail.com>
2024-01-10 23:39:26 +1100

    Day 16 pt 1 solution

--- day-16/day_16.py
+++ day-16/day_16.py
@@ -1,3 +1,4 @@
+from copy import copy, deepcopy
 from enum import Enum
 
 class Direction(Enum):
@@ -6,6 +7,124 @@ class Direction(Enum):
     UP = 'up'
     DOWN = 'down'
 
+def print_grid(grid):
+    grid = deepcopy(grid)
+    g = [''.join(row) for row in grid]
+    print('\n'.join(g))
+    print('============')
+
+
+class Beam:
+    def __init__(self, x, y, direction, caves):
+        self.history = [(x, y, direction)]
+        # list of active beams
+        self.beams = [(x, y, direction)]
+        self.caves = caves
+        self.num_rows = len(caves)
+        self.num_cols = len(caves[0])
+        energised = copy(caves)
+        self.energised = [list(row) for row in energised]
+
+    def run(self):
+        # run all beams to completion
+        while len(self.beams) > 0:
+            updated_beams = []
+            for beam in self.beams:
+                updated_beams += self.move(beam)
+            
+            self.beams = updated_beams
+        
+        print_grid(self.energised)
+
+    def get_num_energised(self):
+        return sum([c == 'X' for r in self.energised for c in r])
+
+    def move(self, beam):
+
+        updated_beams = []
+        x, y, direction = beam
+        self.energised[y][x] = 'X'
+        caves = self.caves
+        if caves[y][x] == '.':
+            if direction == Direction.RIGHT:
+                x += 1
+            elif direction == Direction.LEFT:
+                x -= 1
+            elif direction == Direction.UP:
+                y -= 1
+            elif direction == Direction.DOWN:
+                y += 1
+
+        elif caves[y][x] == '/':
+            if direction == Direction.RIGHT:
+                direction = Direction.UP
+                y -= 1
+            elif direction == Direction.LEFT:
+                direction = Direction.DOWN
+                y += 1
+            elif direction == Direction.UP:
+                direction = Direction.RIGHT
+                x += 1
+            elif direction == Direction.DOWN:
+                direction = Direction.LEFT
+                x -= 1
+
+        elif caves[y][x] == '\\':
+            if direction == Direction.RIGHT:
+                direction = Direction.DOWN
+                y += 1
+            elif direction == Direction.LEFT:
+                direction = Direction.UP
+                y -= 1
+            elif direction == Direction.UP:
+                direction = Direction.LEFT
+                x -= 1
+            elif direction == Direction.DOWN:
+                direction = Direction.RIGHT
+                x += 1
+
+        elif caves[y][x] == '-':
+            if direction == Direction.RIGHT:
+                x += 1
+            elif direction == Direction.LEFT:
+                x -= 1
+            elif direction == Direction.UP or direction == Direction.DOWN:
+                updated_beams.append((x-1, y, direction.LEFT))
+                x += 1
+                direction = Direction.RIGHT
+        elif caves[y][x] == '|':
+            if direction == Direction.UP:
+                y -= 1
+            elif direction == Direction.DOWN:
+                y += 1
+            elif direction == Direction.LEFT or direction == Direction.RIGHT:
+                updated_beams.append((x, y-1, direction.UP))
+                direction = Direction.DOWN
+                y += 1
+        
+        updated_beams.append((x, y, direction))
+        for beam in updated_beams:
+            x, y, direction = beam
+            if self.out_of_bounds(x, y):
+                updated_beams.remove(beam)
+            elif self.in_cycle(x, y, direction):
+                updated_beams.remove(beam)
+            else:
+                self.history.append((x, y, direction))
+        
+        return updated_beams
+
+    def out_of_bounds(self, x, y):
+        return x < 0 or x >= self.num_cols or y < 0 or y >= self.num_rows
+
+    def in_cycle(self, x, y, direction):
+        return (x, y, direction) in self.history
+
+    def __str__(self):
+        return f"({self.x}, {self.y}, {self.direction}, {self.beam_id})"
+
+    def __repr__(self):
+        return f"({self.x}, {self.y}, {self.direction}, {self.beam_id})"
 
 def part_1(lines):
     # parse input
@@ -15,8 +134,7 @@ def part_1(lines):
     print(f"num_rows: {num_rows}, num_cols: {num_cols}")
     energised = [[[] for _ in range(num_cols)] for _ in range(num_rows)]
     # set up start position - each beam is defined by a 4-tuple of x coord, y coord, direction, and beam id
-    latest_beam_id = 0
-    beams = [(0, 0, Direction.RIGHT, latest_beam_id)]
+    beam = Beam(0, 0, Direction.RIGHT, caves)
 
     def print_energised():
         num_energised = 0
@@ -33,102 +151,8 @@ def part_1(lines):
         print()
         return num_energised
     
-    while len(beams) > 0:
-        # update position of each beam
-        new_beams = []
-        for beam in beams:
-            x, y, direction, beam_id = beam
-            # mark as energised 
-            energised[y][x] += [beam_id]
-            print_energised()
-            
-            if caves[y][x] == '.':
-                if direction == Direction.RIGHT:
-                    x += 1
-                elif direction == Direction.LEFT:
-                    x -= 1
-                elif direction == Direction.UP:
-                    y -= 1
-                elif direction == Direction.DOWN:
-                    y += 1
-
-                new_beams.append((x, y, direction, beam_id))
-            elif caves[y][x] == '/':
-                if direction == Direction.RIGHT:
-                    direction = Direction.UP
-                    y -= 1
-                elif direction == Direction.LEFT:
-                    direction = Direction.DOWN
-                    y += 1
-                elif direction == Direction.UP:
-                    direction = Direction.RIGHT
-                    x += 1
-                elif direction == Direction.DOWN:
-                    direction = Direction.LEFT
-                    x -= 1
-
-                new_beams.append((x, y, direction, beam_id))
-            elif caves[y][x] == '\\':
-                if direction == Direction.RIGHT:
-                    direction = Direction.DOWN
-                    y += 1
-                elif direction == Direction.LEFT:
-                    direction = Direction.UP
-                    y -= 1
-                elif direction == Direction.UP:
-                    direction = Direction.LEFT
-                    x -= 1
-                elif direction == Direction.DOWN:
-                    direction = Direction.RIGHT
-                    x += 1
-
-                new_beams.append((x, y, direction, beam_id))
-            elif caves[y][x] == '-':
-                if direction == Direction.RIGHT:
-                    x += 1
-                    new_beams.append((x, y, direction, beam_id))
-                elif direction == Direction.LEFT:
-                    x -= 1
-                    new_beams.append((x, y, direction, beam_id))
-                elif direction == Direction.UP:
-                    new_beams.append((x-1, y, direction.LEFT, beam_id))
-                    latest_beam_id += 1
-                    new_beams.append((x+1, y, direction.RIGHT, latest_beam_id))
-                elif direction == Direction.DOWN:
-                    new_beams.append((x-1, y, direction.LEFT, beam_id))
-                    latest_beam_id += 1
-                    new_beams.append((x+1, y, direction.RIGHT, latest_beam_id))
-            elif caves[y][x] == '|':
-                if direction == Direction.UP:
-                    y -= 1
-                    new_beams.append((x, y, direction, beam_id))
-                elif direction == Direction.DOWN:
-                    y += 1
-                    new_beams.append((x, y, direction, beam_id))
-                elif direction == Direction.LEFT:
-                    new_beams.append((x, y-1, direction.UP, beam_id))
-                    latest_beam_id += 1
-                    new_beams.append((x, y+1, direction.DOWN, latest_beam_id))
-                elif direction == Direction.RIGHT:
-                    new_beams.append((x, y-1, direction.UP, beam_id))
-                    latest_beam_id += 1
-                    new_beams.append((x, y+1, direction.DOWN, latest_beam_id))
-
-            # check if beams have left grid
-            # if so, remove them from the list
-            beams = new_beams
-            for beam in beams:
-                try:
-                    x, y, direction, beam_id = beam
-                except ValueError:
-                    print(beam)
-                if x < 0 or x >= num_cols or y < 0 or y >= num_rows:
-                    beams.remove(beam)
-                elif beam_id in energised[y][x]:
-                    beams.remove(beam)
-        
-
-    return num_energised
+    beam.run()
+    return beam.get_num_energised()
     
     
 def part_2(lines):
@@ -146,8 +170,8 @@ if __name__ == '__main__':
     print("Part 1 ======================")
     test_vals = part_1(test_lines)
     print(f"Test output: {test_vals}")
-    #input_vals = part_1(input_lines)
-    #print(f"Real output: {input_vals}")
+    input_vals = part_1(input_lines)
+    print(f"Real output: {input_vals}")
 
     print("Part 2 ======================")
     test_vals = part_2(test_lines)
