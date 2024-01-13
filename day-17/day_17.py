954800a7c797df5e7742f23f04755659eda4bf9b
James Sinclair <sinclair.j@gmail.com>
2024-01-13 16:42:50 +1100

    Day 17 pt 1 attempt
    - added direction to simplify consecutive step computation
    - still very slow on large input

--- day-17/day_17.py
+++ day-17/day_17.py
@@ -16,7 +16,6 @@ def part_1(lines):
     start_location = (0, 0)
     end_location = (num_cols - 1, num_rows - 1)
     
-    #, [], 0, 0)
     # state on grid is (path, heat_loss)
     # path is a list of tuples (x, y, direction)
     # heat_loss is the sum of the heat loss of all the tiles in the path
@@ -24,70 +23,52 @@ def part_1(lines):
 
     # conduct a breadth first search from the start location to the end location to determine the path with minimal
     # heat loss, without taking more than 3 steps in a straight line
-    queue = [(0, [(0, 0)])]
+    queue = [(0, [(0, 0)], Direction.RIGHT, 0), (0, [(0, 0)], Direction.DOWN, 0)]
     heapq.heapify(queue)
-    finished_paths = []
 
-    def get_straight_line_steps(path):
-        if len(path) == 0:
-            return 0
-        else:
-            x, y = path[-1]
-            ind = len(path) - 2
-            num_steps = 0
-            # case 2: we're moving left or right
-            if x == path[ind][0]:
-                while ind >= 0 and x == path[ind][0]:
-                    num_steps += 1
-                    ind -= 1
-                
-            # case 1: we're moving up or down
-            elif y == path[ind][1]:
-                while ind >= 0 and y == path[ind][1]:
-                    num_steps += 1
-                    ind -= 1
-
-            return num_steps
-    
-    def visited(x, y, path):
-        for x1, y1 in path[:-1]:
-            if x == x1 and y == y1:
-                return True
-        return False
-    
     def out_of_bounds(x, y):
         return x < 0 or x >= num_cols or y < 0 or y >= num_rows
     
-    def exceeded_straight_line_steps(path):
-        return get_straight_line_steps(path) > max_straight_line_steps
-
     explored = set()
 
+    i = 0
     while len(queue) > 0:
         current_location = heapq.heappop(queue)
-        heat_loss, path = current_location
-        print(heat_loss)
-        x, y = path[-1]
+        heat_loss, path, direction, num_steps = current_location
 
+        if i % 10000:
+            print(heat_loss)
+        i += 1
+
+        #print(path)
+        x, y = path[-1]
         # check if we've reached the end
         if x == end_location[0] and y == end_location[1]:
             print(f"Found path: {path}, heat loss: {heat_loss}")
             finished_path = path
-            break
+            min_heat_loss = heat_loss
+            break 
 
         # move in different directions
         explored.add((x, y))
-        new_positions = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
-        for new_x, new_y in new_positions:
+        new_positions = [(x + 1, y, Direction.RIGHT), (x - 1, y, Direction.LEFT), 
+                         (x, y + 1, Direction.DOWN), (x, y - 1, Direction.UP)]
+        for new_x, new_y, new_dir in new_positions:
+            if out_of_bounds(new_x, new_y):
+                continue
             if (new_x, new_y) in explored:
-                print(f"explored {new_x}, {new_y}")
                 continue
-            if out_of_bounds(new_x, new_y) or visited(new_x, new_y, path) or exceeded_straight_line_steps(path + [(new_x, new_y)]):
-                continue
+
+            if new_dir == direction:
+                new_num_steps = num_steps + 1
+                if new_num_steps > max_straight_line_steps:
+                    continue
             else:
-                # insert new state into queue based on current heat loss
-                new_heat_loss = heat_loss + heat_loss_map[y][x]
-                heapq.heappush(queue, (new_heat_loss, path + [(new_x, new_y)]))
+                new_num_steps = 1
+
+            # insert new state into queue based on current heat loss
+            new_heat_loss = heat_loss + heat_loss_map[new_y][new_x]
+            heapq.heappush(queue, (new_heat_loss, path + [(new_x, new_y)], new_dir, new_num_steps))
 
     path_map = deepcopy(heat_loss_map)
     min_heat_loss = 0
