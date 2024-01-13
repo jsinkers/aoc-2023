7b4336e1f4e7a166857367c4b0a527a40bb7cfc1
James Sinclair <sinclair.j@gmail.com>
2024-01-13 15:43:22 +1100

    Day 17 pt 1 attempt
    - works on test but not on full size.

--- day-17/day_17.py
+++ day-17/day_17.py
@@ -1,5 +1,6 @@
+from copy import deepcopy
 import re
-
+import heapq
         
 class Direction:
     RIGHT = 'right'
@@ -23,72 +24,84 @@ def part_1(lines):
 
     # conduct a breadth first search from the start location to the end location to determine the path with minimal
     # heat loss, without taking more than 3 steps in a straight line
-    queue = [([(0, 0, Direction.RIGHT)], 0), ((0,0, Direction.DOWN), 0)]
+    queue = [(0, [(0, 0)])]
+    heapq.heapify(queue)
     finished_paths = []
 
     def get_straight_line_steps(path):
         if len(path) == 0:
             return 0
         else:
-            x, y, direction = path[-1]
-            if direction == Direction.RIGHT:
-                return 1 + get_straight_line_steps(path[:-1])
-            else:
-                return 0
+            x, y = path[-1]
+            ind = len(path) - 2
+            num_steps = 0
+            # case 2: we're moving left or right
+            if x == path[ind][0]:
+                while ind >= 0 and x == path[ind][0]:
+                    num_steps += 1
+                    ind -= 1
+                
+            # case 1: we're moving up or down
+            elif y == path[ind][1]:
+                while ind >= 0 and y == path[ind][1]:
+                    num_steps += 1
+                    ind -= 1
+
+            return num_steps
     
     def visited(x, y, path):
-        for x1, y1, _ in path[:-1]:
+        for x1, y1 in path[:-1]:
             if x == x1 and y == y1:
                 return True
         return False
     
     def out_of_bounds(x, y):
         return x < 0 or x >= num_cols or y < 0 or y >= num_rows
+    
+    def exceeded_straight_line_steps(path):
+        return get_straight_line_steps(path) > max_straight_line_steps
+
+    explored = set()
 
     while len(queue) > 0:
-        print(len(queue))
-        current_location = queue.pop(0)
-        path, heat_loss = current_location
-        x, y, _ = path[-1]
+        current_location = heapq.heappop(queue)
+        heat_loss, path = current_location
+        print(heat_loss)
+        x, y = path[-1]
 
-        # check if we're out of bounds
-        if out_of_bounds(x, y):
-            #print(f"Out of bounds {x},{y}")
-            continue
-
-        heat_loss += heat_loss_map[y][x]
-        #print(f"Current location: {x}, {y}, {direction}, heat loss: {heat_loss}")
-        min_heat_loss = None
-
-        # check if we've already visited this location
-        if visited(x, y, path):
-            print(f"Visited {x},{y}")
-            continue
-
-        # check if we've already exceeded the known minimum heat loss
-        if min_heat_loss is not None and heat_loss > min_heat_loss:
-            print(f"Exceeded min heat loss {heat_loss} > {min_heat_loss}")
-            continue
-
-        # check if we've taken too many straight line steps
-        current_straight_line_steps = get_straight_line_steps(path)
-        if current_straight_line_steps > max_straight_line_steps:
-            continue
-    
         # check if we've reached the end
         if x == end_location[0] and y == end_location[1]:
             print(f"Found path: {path}, heat loss: {heat_loss}")
-            finished_paths.append((path, heat_loss))
-            if min_heat_loss is None or heat_loss < min_heat_loss:
-                min_heat_loss = heat_loss
-            continue
+            finished_path = path
+            break
 
         # move in different directions
-        new_states = [(path + [(y + 1, x, Direction.DOWN)], heat_loss),
-                        (path + [(y - 1, x, Direction.UP)], heat_loss),
-                        (path + [(y, x + 1, Direction.RIGHT)], heat_loss),
-                        (path + [(y, x - 1, Direction.LEFT)], heat_loss)]
-        queue += new_states
+        explored.add((x, y))
+        new_positions = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+        for new_x, new_y in new_positions:
+            if (new_x, new_y) in explored:
+                print(f"explored {new_x}, {new_y}")
+                continue
+            if out_of_bounds(new_x, new_y) or visited(new_x, new_y, path) or exceeded_straight_line_steps(path + [(new_x, new_y)]):
+                continue
+            else:
+                # insert new state into queue based on current heat loss
+                new_heat_loss = heat_loss + heat_loss_map[y][x]
+                heapq.heappush(queue, (new_heat_loss, path + [(new_x, new_y)]))
+
+    path_map = deepcopy(heat_loss_map)
+    min_heat_loss = 0
+    for path in finished_path:
+        x, y = path
+        if x == 0 and y == 0:
+            continue
+        min_heat_loss += heat_loss_map[y][x]
+        path_map[y][x] = '.'
+        
+    
+    for row in path_map:
+        row = [str(x) for x in row]
+        print(''.join(row))
 
     return min_heat_loss
     
