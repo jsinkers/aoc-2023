0e110b138bbd78672d329805073826fb687a5abc
James Sinclair <sinclair.j@gmail.com>
2024-01-17 12:46:56 +1100

    Day 19 pt 1 soln

--- day-19/day_19.py
+++ day-19/day_19.py
@@ -4,6 +4,82 @@ import re
 
 def part_1(lines):
     total = 0
+    
+    workflows = True
+    workflow_dict = {}
+    objects = []
+    obj_strings = []
+    for line in lines:
+        line = line.strip()
+        if line == '':
+            workflows = False
+        elif workflows:
+            # parse workflows
+            matches = re.findall(r'(.*){(.*)}', line)
+            name, rules = matches[0]
+            rules = rules.split(',')
+
+            workflow_dict[name] = []
+            for rule in rules:
+                if ":" in rule:
+                    matches = re.findall(r'(.+)([\<\>])(\d+):(.+)', rule)
+                    attr, op, val, next_rule = matches[0]
+                    val = int(val)
+                    workflow_dict[name].append((attr, op, val, next_rule))
+                else:
+                    workflow_dict[name].append((rule,))
+        
+        else:
+            # parse ratings
+            matches = re.findall(r'((.)=(\d+))', line)
+            new_object = {}
+            obj_strings.append(line)
+            for match in matches:
+                attr, val = match[1], match[2]
+                val = int(val)
+                new_object[attr] = val
+            
+            objects.append(new_object)
+
+    # sort parts
+    accepted_parts = []
+    for obj in objects:
+        print(obj, end='')
+        workflow_name = 'in'
+        accepted = None
+        while True:
+            print(f"-> {workflow_name} ", end='')
+            if workflow_name == 'R':
+                #accepted = False
+                break
+            elif workflow_name == 'A':
+                #accepted = True
+                accepted_parts.append(obj)
+                break
+
+            workflow = workflow_dict[workflow_name]
+            #print(workflow)
+            for rule in workflow:
+                #print(rule)
+                if len(rule) == 1:
+                    workflow_name = rule[0]
+                    break
+
+                attr, op, val, next_rule = rule
+                if op == '<':
+                    if obj[attr] < val:
+                        workflow_name = next_rule
+                        break
+                elif op == '>':
+                    if obj[attr] > val:
+                        workflow_name = next_rule
+                        break
+        
+        print()
+                
+    # add rating numbers of all accepted parts
+    total = sum([sum(obj.values()) for obj in accepted_parts])
+
     return total
     
 def part_2(lines):
