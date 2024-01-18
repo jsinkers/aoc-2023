15d53297243f489ad5cfd83f2121913485933264
James Sinclair <sinclair.j@gmail.com>
2024-01-19 00:05:04 +1100

    Day 20 attempt 1
    - works on test cases but not on real input; have noticed issue with parent detection for conjunction modules

--- day-20/day_20.py
+++ day-20/day_20.py
@@ -15,7 +15,11 @@ class Module:
     def get_output(self, pulse):
         return [(self.name, dest_module, pulse) for dest_module in self.dest_modules]
 
-# Flip-flop modules (prefix %) are either on or off; they are initially off. If a flip-flop module receives a high pulse, it is ignored and nothing happens. However, if a flip-flop module receives a low pulse, it flips between on and off. If it was off, it turns on and sends a high pulse. If it was on, it turns off and sends a low pulse.
+# Flip-flop modules (prefix %) are either on or off; they are initially off. If
+# a flip-flop module receives a high pulse, it is ignored and nothing happens.
+# However, if a flip-flop module receives a low pulse, it flips between on and
+# off. If it was off, it turns on and sends a high pulse. If it was on, it turns
+# off and sends a low pulse.
 class FlipFlopModule(Module):
     def __init__(self, name):
         self.on = False
@@ -30,10 +34,9 @@ class FlipFlopModule(Module):
             else:
                 self.output = Pulse.LOW
             
-            #print(f"FlipFlopModule {self.name} on: {self.on}, input: {pulse}, output: {self.output}")
+            #print(f"\tFlipFlopModule {self.name} on: {self.on}, input: {pulse}, output: {self.output}")
             return super().get_output(self.output)
         else:
-            self.output = None
             return []
         
     
@@ -63,15 +66,17 @@ class ConjunctionModule(Module):
         pulse1 = self.parent_module_1.output
         pulse2 = self.parent_module_2.output
 
+        # update memory
+        self.last_pulse_1 = pulse1
+        self.last_pulse_2 = pulse2
+
         # determine output 
         self.output = Pulse.HIGH
         if self.last_pulse_1 == Pulse.HIGH and self.last_pulse_2 == Pulse.HIGH:
             self.output = Pulse.LOW
 
-        # update memory
-        self.last_pulse_1 = pulse1
-        self.last_pulse_2 = pulse2
 
+        #print(f"\tConjunctionModule {self.name} pulse1: {pulse1}, pulse2: {pulse2}, output: {self.output}")
         return super().get_output(self.output)
 
 # broadcast module
@@ -113,25 +118,41 @@ def part_1(lines):
         else:
             # broadcast
             network[src].add_dest_modules(dest)
+
+        for d in dest:
+            if d not in network:
+                network[d] = None
         
     # populate conjunction modules
     for conj_module in conj_modules:
-        conj_module.add_parent_modules([m for m in network.values() if conj_module.name in m.dest_modules])
+        parents = [m for m in network.values() if m is not None]
+        parents = [m for m in parents if conj_module.name in m.dest_modules]
+        print(f"Conjunction {conj_module.name} parents: {[p.name for p in parents]}")
+        conj_module.add_parent_modules(parents)
     
     # queue of pulses to process
-    pulses = [('button', 'broadcaster', Pulse.LOW)]
-    counter = 0
-    while len(pulses) > 0:
-        counter += 1
-        if counter > 10:
-            break
-        # process pulse
-        src, dest, pulse = pulses.pop(0)
-        print(f"{src} -{pulse}-> {dest}")
-        new_pulses = network[dest].input(pulse)
-        pulses.extend(new_pulses)
-        #print(pulses)
-    return counter
+    print("========================")
+    num_low, num_high = 0, 0
+    for i in range(1000):
+        #print(f"Step {i} ==============")
+        pulses = [('button', 'broadcaster', Pulse.LOW)]
+        while len(pulses) > 0:
+            # process pulse
+            src, dest, pulse = pulses.pop(0)
+            if pulse == Pulse.LOW:
+                num_low += 1
+            else:
+                num_high += 1
+            #print(f"{src} -{pulse}-> {dest}")
+            if network[dest] is None:
+                #print(f"Network {dest} is None")
+                continue
+            new_pulses = network[dest].input(pulse)
+            pulses.extend(new_pulses)
+            #print(pulses)
+
+    print(f"num_low: {num_low}, num_high: {num_high}")
+    return num_low*num_high
     
 def part_2(lines):
     total = 0
@@ -142,14 +163,28 @@ if __name__ == '__main__':
     with open('test.txt', 'r') as f:
         test_lines = f.readlines()
 
+    with open('test_2.txt', 'r') as f:
+        test_2_lines = f.readlines()
+
+    #with open('test_3.txt', 'r') as f:
+    #    test_3_lines = f.readlines()
+
+    #with open('test_4.txt', 'r') as f:
+    #    test_4_lines = f.readlines()
+
+    #with open('test_5.txt', 'r') as f:
+    #    test_5_lines = f.readlines()
+
     with open('input.txt', 'r') as f:
         input_lines = f.readlines()
     
     print("Part 1 ======================")
     test_vals = part_1(test_lines)
-    print(f"Test output: {test_vals}")
-    #input_vals = part_1(input_lines)
-    #print(f"Real output: {input_vals}")
+    print(f"Test output: {test_vals}\n")
+    test_vals = part_1(test_2_lines)
+    print(f"Test output: {test_vals}\n")
+    input_vals = part_1(input_lines)
+    print(f"Real output: {input_vals}")
 
     #print("Part 2 ======================")
     #test_vals = part_2(test_lines)
