6eb8f67a886050169b734c3cb0c4d48b27db885f
James Sinclair <sinclair.j@gmail.com>
2024-01-18 23:31:59 +1100

    Day 20 pt 1 progress

--- day-20/day_20.py
+++ day-20/day_20.py
@@ -1,36 +1,42 @@
 import re
 
 class Pulse:
-    LOW = False
-    HIGH = True
+    LOW = "low"
+    HIGH = "high"
 
 class Module:
     def __init__(self, name):
         self.dest_modules = []
         self.name = name
 
-    def add_dest_module(self, dest_module):
-        self.dest_modules.append(dest_module)
+    def add_dest_modules(self, dest_modules):
+        self.dest_modules += dest_modules
     
-    def output(self, pulse):
+    def get_output(self, pulse):
         return [(self.name, dest_module, pulse) for dest_module in self.dest_modules]
 
 # Flip-flop modules (prefix %) are either on or off; they are initially off. If a flip-flop module receives a high pulse, it is ignored and nothing happens. However, if a flip-flop module receives a low pulse, it flips between on and off. If it was off, it turns on and sends a high pulse. If it was on, it turns off and sends a low pulse.
 class FlipFlopModule(Module):
     def __init__(self, name):
         self.on = False
+        self.output = None
         super().__init__(name)
 
     def input(self, pulse: Pulse):
         if pulse == Pulse.LOW:
             self.on = not self.on
             if self.on:
-                return Pulse.HIGH
+                self.output = Pulse.HIGH
             else:
-                return Pulse.LOW
+                self.output = Pulse.LOW
+            
+            #print(f"FlipFlopModule {self.name} on: {self.on}, input: {pulse}, output: {self.output}")
+            return super().get_output(self.output)
         else:
-            return None
+            self.output = None
+            return []
         
+    
 # Conjunction modules (prefix &) remember the type of the most recent pulse
 # received from each of their connected input modules; they initially default to
 # remembering a low pulse for each input. When a pulse is received, the
@@ -42,49 +48,90 @@ class ConjunctionModule(Module):
     def __init__(self, name):
         self.last_pulse_1 = Pulse.LOW
         self.last_pulse_2 = Pulse.LOW
-        self.super().__init__(name)
+        self.output = None
+        super().__init__(name)
     
-    def input(self, pulse1, pulse2):
+    def add_parent_modules(self, parent_modules):
+        if len(parent_modules) == 1:
+            self.parent_module_1 = parent_modules[0]
+            self.parent_module_2 = parent_modules[0]
+        else:
+            self.parent_module_1 = parent_modules[0]
+            self.parent_module_2 = parent_modules[1]
+
+    def input(self, pulse1):
+        pulse1 = self.parent_module_1.output
+        pulse2 = self.parent_module_2.output
+
         # determine output 
-        output = Pulse.HIGH
+        self.output = Pulse.HIGH
         if self.last_pulse_1 == Pulse.HIGH and self.last_pulse_2 == Pulse.HIGH:
-            output = Pulse.LOW
+            self.output = Pulse.LOW
 
         # update memory
         self.last_pulse_1 = pulse1
         self.last_pulse_2 = pulse2
 
-        return super().output(output)
+        return super().get_output(self.output)
 
 # broadcast module
 class BroadcastModule(Module):
     def input(self, pulse):
-        return super().output(pulse)
+        return super().get_output(pulse)
         
 def part_1(lines):
     # parse input
     total = 0
 
     network = {}
-    broadcastModule = BroadcastModule('broadcast')
+    broadcastModule = BroadcastModule('broadcaster')
     network[broadcastModule.name] = broadcastModule
-    modA = FlipFlopModule('a')
-    modB = FlipFlopModule('b')
-    network[modA.name] = modA
-    network[modB.name] = modB
-    broadcastModule.add_dest_module('a')
-    broadcastModule.add_dest_module('b')
+
+    pattern = r'([%&])?(.*) -> (.*)'
+    conj_modules = []
+
+    for line in lines:
+        line = line.strip()
+        matches = re.findall(pattern, line)
+        match = matches[0]
+
+        module_type, src, dest = match
+        dest = dest.split(',')
+        dest = [d.strip() for d in dest]
+        print(f"Adding {module_type} -{src}-> {dest}")
+        if module_type == '%':
+            # flip flop
+            flipFlopModule = FlipFlopModule(src)
+            network[src] = flipFlopModule
+            flipFlopModule.add_dest_modules(dest)
+        elif module_type == '&':
+            # conjunction
+            conjunctionModule = ConjunctionModule(src)
+            network[src] = conjunctionModule
+            conjunctionModule.add_dest_modules(dest)
+            conj_modules.append(conjunctionModule)
+        else:
+            # broadcast
+            network[src].add_dest_modules(dest)
+        
+    # populate conjunction modules
+    for conj_module in conj_modules:
+        conj_module.add_parent_modules([m for m in network.values() if conj_module.name in m.dest_modules])
     
     # queue of pulses to process
-    pulses = [('button', 'broadcast', Pulse.LOW)]
+    pulses = [('button', 'broadcaster', Pulse.LOW)]
+    counter = 0
     while len(pulses) > 0:
+        counter += 1
+        if counter > 10:
+            break
         # process pulse
-        src, dest, pulse = pulses.pop()
+        src, dest, pulse = pulses.pop(0)
         print(f"{src} -{pulse}-> {dest}")
-        network[dest].input(pulse)
-        pulses.extend(network[dest].output(pulse))
-        print(pulses)
-    return total
+        new_pulses = network[dest].input(pulse)
+        pulses.extend(new_pulses)
+        #print(pulses)
+    return counter
     
 def part_2(lines):
     total = 0
@@ -101,11 +148,11 @@ if __name__ == '__main__':
     print("Part 1 ======================")
     test_vals = part_1(test_lines)
     print(f"Test output: {test_vals}")
-    input_vals = part_1(input_lines)
-    print(f"Real output: {input_vals}")
+    #input_vals = part_1(input_lines)
+    #print(f"Real output: {input_vals}")
 
-    print("Part 2 ======================")
-    test_vals = part_2(test_lines)
-    print(f"Test output: {test_vals}")
-    input_vals = part_2(input_lines)
-    print(f"Real output: {input_vals}")
+    #print("Part 2 ======================")
+    #test_vals = part_2(test_lines)
+    #print(f"Test output: {test_vals}")
+    #input_vals = part_2(input_lines)
+    #print(f"Real output: {input_vals}")
