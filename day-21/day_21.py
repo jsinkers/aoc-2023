5dcbf6c19ace862de717c2b4912924542ab8add4
James Sinclair <sinclair.j@gmail.com>
2024-01-20 00:44:11 +1100

    Day 21 pt 2 attempt
    - brute force - working but v slow. Checked up to 500
    - I think you are left with positions in a diamond out from the beginning - but you need to carve out places not reachable in the required number of steps
    - many of the positions are just moving back and forth - could just examine the frontier of newly explored points. this should remain small

--- day-21/day_21.py
+++ day-21/day_21.py
@@ -49,9 +49,40 @@ def part_1(lines, num_steps=64):
    
     return len(current_positions) 
 
-def part_2(lines):
-    total = 0
-    return total
+def part_2(lines, num_steps=26501365):
+    garden = []
+    for i, line in enumerate(lines):
+        line = line.strip()
+        garden.append(list(line))
+        if 'S' in line:
+            start = (i, line.index('S'))
+    
+    num_rows = len(garden)
+    num_cols = len(garden[0])
+
+    def print_garden(positions):
+        grid = deepcopy(garden) 
+        for x, y in positions:
+            grid[y][x] = 'O'
+        print_grid(grid)
+
+    current_positions = set([start])
+    for i in range(num_steps):
+        new_positions = set()
+        for position in current_positions:
+            x, y = position
+            # try to move up, down, left, right
+            candidate_posns = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+            for x, y in candidate_posns:
+                if garden[y % num_rows][x % num_cols] in '.S':
+                    new_positions.add((x, y))
+
+        current_positions = new_positions
+        print(f"Step {i+1}: num positions={len(current_positions)}")
+
+    #print_garden(current_positions)
+   
+    return len(current_positions) 
     
 
 if __name__ == '__main__':
@@ -68,7 +99,7 @@ if __name__ == '__main__':
     print(f"Real output: {input_vals}")
 
     #print("Part 2 ======================")
-    #test_vals = part_2(test_lines)
-    #print(f"Test output: {test_vals}")
+    test_vals = part_2(test_lines, num_steps=1000)
+    print(f"Test output: {test_vals}")
     #input_vals = part_2(input_lines)
     #print(f"Real output: {input_vals}")
