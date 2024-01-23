99b5f2603530e9b29ae19827453a15fadc74ad28
James Sinclair <sinclair.j@gmail.com>
2024-01-23 23:30:51 +1100

    Day 22 pt 2 attempt

--- day-22/day_22.py
+++ day-22/day_22.py
@@ -1,3 +1,4 @@
+from functools import cache
 import re
 from copy import deepcopy
 import heapq
@@ -166,8 +167,162 @@ def part_1(lines, convert=True):
     return num_to_disintegrate
     
 def part_2(lines):
-    total = 0
-    return total
+    # parse input
+    bricks = []
+    #heapq.heapify(bricks)
+
+    max_x, max_y, max_z = 0, 0, 0
+    for line in lines:
+        coords = line.split('~')
+        coords1 = [int(x) for x in coords[0].split(',')]
+        coords2 = [int(x) for x in coords[1].split(',')]
+        x1, y1, z1 = coords1
+        x2, y2, z2 = coords2
+        x1, x2 = min(x1, x2), max(x1, x2)
+        y1, y2 = min(y1, y2), max(y1, y2)
+        z1, z2 = min(z1, z2), max(z1, z2)
+        coords1 = (x1, y1, z1)
+        coords2 = (x2, y2, z2)
+        # insert brick using min_z as priority
+        #heapq.heappush(bricks, (z1, coords1, coords2))
+        bricks.append((coords1, coords2))
+        max_x = max(max_x, x1, x2)
+        max_y = max(max_y, y1, y2)
+        max_z = max(max_z, z1, z2)
+    
+    # sort bricks by z 
+    bricks.sort(key=lambda x: x[0][2])
+    # now move bricks down until they hit the floor or another brick
+    # make an xy grid the size of max_x, max_y, initially on the floor
+    height_envelope = [[0 for _ in range(max_x+1)] for _ in range(max_y+1)]
+    # this will act as an envelope of the top of all already moved bricks
+    moved_bricks = []
+    for i in range(len(bricks)):
+        #min_z, coords1, coords2 = heapq.heappop(bricks)
+        coords1, coords2 = bricks.pop(0)
+        x1, y1, z1 = coords1
+        x2, y2, z2 = coords2
+        #print(f"Brick {i}: {coords1} ~ {coords2}")
+        #print(height_envelope)
+        # look at the max of the envelope restricted to the x, y coords of the brick
+        envelope_max = 0
+        for x in range(x1, x2+1):
+            for y in range(y1, y2+1):
+                envelope_max = max(envelope_max, height_envelope[y][x])
+
+        # move the brick down to that height + 1
+        diff = z1 - envelope_max - 1
+        #print(diff)
+        z1 -= diff
+        z2 -= diff
+
+        # reset the envelope to account for the brick
+        for x in range(x1, x2+1):
+            for y in range(y1, y2+1):
+                #print(f"{x}, {y}")
+                height_envelope[y][x] = max(z1, z2, height_envelope[y][x])
+        
+        # add the brick to the moved bricks
+        moved_bricks.append(((x1, y1, z1), (x2, y2, z2)))
+
+
+    # now see if the brick is safe to disintegrate - i.e. is not supporting any bricks directly above it
+    # supported_by is a lookup of all bricks that a given brick is supported by
+    supported_by = [[] for _ in range(len(moved_bricks))]
+    # supporting_list is a lookup of all bricks that a given brick is supporting
+    supporting_list = [[] for _ in range(len(moved_bricks))]
+    for i, brick in enumerate(moved_bricks):
+        coords1, coords2 = brick
+        x1, y1, z1 = coords1
+        x2, y2, z2 = coords2
+        supporting = False
+        if i == len(moved_bricks) - 1:
+            continue
+
+        for j, brick2 in enumerate(moved_bricks[i+1:]):
+            ind = i + j + 1
+            #print(f"checking brick {i} against brick {ind}")
+            coords3, coords4 = brick2
+            x3, y3, z3 = coords3
+            x4, y4, _  = coords4
+            # check if brick2 is directly above brick
+            if z3 == z2 + 1:
+                # is there overlap in x and y?
+                for x in range(x3, x4+1):
+                    for y in range(y3, y4+1):
+                        if x1 <= x <= x2 and y1 <= y <= y2:
+                            supporting = True
+                            break
+                    if supporting:
+                        break
+
+            if supporting:
+                #print(f"brick {i} supports brick {ind}")
+                supported_by[ind].append(i)
+                supporting_list[i].append(ind)
+                supporting = False
+            
+    num_bricks = len(supporting_list)
+    #supported_by = [set(s) for s in supported_by]
+    #supporting_list = [set(s) for s in supporting_list]
+    # check supported by and supporting list describe reverse of same graph
+    for i in range(num_bricks):
+        for j in supporting_list[i]:
+            if i not in supported_by[j]:
+                raise ValueError(f"Brick {i} supports {j} but {j} is not supported by {i}")
+
+
+    def num_to_fall(brick_to_remove):
+        # going from the brick to remove, traverse the graph of bricks that it supports, and remove
+        # the edge
+        supported_by_copy = deepcopy(supported_by)
+        queue = [brick_to_remove]
+        counter = 0
+        visited = []
+
+        # bfs over the supporting graph
+        while len(queue) > 0:
+            b = queue.pop(0)
+            if b in visited:
+                continue
+
+            # check if brick is supported by any bricks
+            print(f"Considering brick {b}: supported by {supported_by_copy[b]}")
+            if len(supported_by_copy[b]) == 0:
+                # if not increment the counter and mark visited
+                counter += 1
+                visited.append(b)
+                print(f"Brick {b} falls, count={counter}")
+
+                # as it's going to fall, we now need to remove this brick as a support from 
+                # the bricks it previously supported
+                for s in supporting_list[b]:
+                    if b in supported_by_copy[s]:
+                        supported_by_copy[s].remove(b)
+                        queue.append(s)
+            
+        return counter
+        #return [len(s) for s in supported_by_copy].count(0) + 1
+        
+    def num_on_ground(brick):
+        count = 0
+        for b in moved_bricks:
+            coords1, coords2 = b
+            x1, y1, z1 = coords1
+            x2, y2, z2 = coords2
+            if z1 == 1 and b != brick:
+                count += 1
+        
+        return count
+            
+    max_to_fall = 0
+    for brick in range(num_bricks):
+        #print(f"{brick}: supports {supporting_list[brick]}, supported_by {supported_by[brick]}")
+        count = num_to_fall(brick) #- num_on_ground(brick)
+        print(f"Brick {brick} has {count} bricks to fall")
+        max_to_fall = max(max_to_fall, count)
+
+    return max_to_fall
     
 
 if __name__ == '__main__':
@@ -198,8 +353,8 @@ if __name__ == '__main__':
     input_vals = part_1(input_lines)
     print(f"Real output: {input_vals}")
 
-    # print("Part 2 ======================")
-    # test_vals = part_2(test_lines)
-    # print(f"Test output: {test_vals}")
-    # input_vals = part_2(input_lines)
-    # print(f"Real output: {input_vals}")
+    print("Part 2 ======================")
+    test_vals = part_2(test_lines)
+    print(f"Test output: {test_vals}")
+    input_vals = part_2(input_lines)
+    print(f"Real output: {input_vals}")
