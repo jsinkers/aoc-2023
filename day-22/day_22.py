b5c49397d1276a91add2a545ccdea68d7f1ec354
James Sinclair <sinclair.j@gmail.com>
2024-01-23 17:42:29 +1100

    Day 22 pt 1 solution

--- day-22/day_22.py
+++ day-22/day_22.py
@@ -3,31 +3,40 @@ from copy import deepcopy
 import heapq
 
 
-def part_1(lines):
+def part_1(lines, convert=True):
     # parse input
     bricks = []
-    heapq.heapify(bricks)
+    #heapq.heapify(bricks)
 
     max_x, max_y, max_z = 0, 0, 0
     for line in lines:
         coords = line.split('~')
         coords1 = [int(x) for x in coords[0].split(',')]
         coords2 = [int(x) for x in coords[1].split(',')]
-        min_z = min(coords1[2], coords2[2])
+        x1, y1, z1 = coords1
+        x2, y2, z2 = coords2
+        x1, x2 = min(x1, x2), max(x1, x2)
+        y1, y2 = min(y1, y2), max(y1, y2)
+        z1, z2 = min(z1, z2), max(z1, z2)
+        coords1 = (x1, y1, z1)
+        coords2 = (x2, y2, z2)
         # insert brick using min_z as priority
-        heapq.heappush(bricks, (min_z, coords1, coords2))
-        #bricks.append((coords1, coords2))
-        max_x = max(max_x, coords1[0], coords2[0])
-        max_y = max(max_y, coords1[1], coords2[1])
-        max_z = max(max_z, coords1[2], coords2[2])
+        #heapq.heappush(bricks, (z1, coords1, coords2))
+        bricks.append((coords1, coords2))
+        max_x = max(max_x, x1, x2)
+        max_y = max(max_y, y1, y2)
+        max_z = max(max_z, z1, z2)
     
+    # sort bricks by z 
+    bricks.sort(key=lambda x: x[0][2])
     # now move bricks down until they hit the floor or another brick
     # make an xy grid the size of max_x, max_y, initially on the floor
     height_envelope = [[0 for _ in range(max_x+1)] for _ in range(max_y+1)]
     # this will act as an envelope of the top of all already moved bricks
     moved_bricks = []
     for i in range(len(bricks)):
-        min_z, coords1, coords2 = heapq.heappop(bricks)
+        #min_z, coords1, coords2 = heapq.heappop(bricks)
+        coords1, coords2 = bricks.pop(0)
         x1, y1, z1 = coords1
         x2, y2, z2 = coords2
         #print(f"Brick {i}: {coords1} ~ {coords2}")
@@ -39,7 +48,7 @@ def part_1(lines):
                 envelope_max = max(envelope_max, height_envelope[y][x])
 
         # move the brick down to that height + 1
-        diff = min_z - envelope_max - 1
+        diff = z1 - envelope_max - 1
         #print(diff)
         z1 -= diff
         z2 -= diff
@@ -93,18 +102,18 @@ def part_1(lines):
         print('\n'.join(g))
         print('============')
 
-    #print(moved_bricks)
-    print_grid('xz', convert=False)
-    print_grid('yz', convert=False)
+    #print_grid('xz', convert)
+    #print_grid('yz', convert)
 
     # now see if the brick is safe to disintegrate - i.e. is not supporting any bricks directly above it
+    # supported_by is a lookup of all bricks that a given brick is supported by
     supported_by = [[] for _ in range(len(moved_bricks))]
+    # supporting_list is a lookup of all bricks that a given brick is supporting
     supporting_list = [[] for _ in range(len(moved_bricks))]
     for i, brick in enumerate(moved_bricks):
         coords1, coords2 = brick
         x1, y1, z1 = coords1
         x2, y2, z2 = coords2
-        # brick_envelope
         supporting = False
         if i == len(moved_bricks) - 1:
             continue
@@ -114,15 +123,9 @@ def part_1(lines):
             #print(f"checking brick {i} against brick {ind}")
             coords3, coords4 = brick2
             x3, y3, z3 = coords3
-            x4, y4, z4 = coords4
+            x4, y4, _  = coords4
             # check if brick2 is directly above brick
-            if z3 == z2:
-                # at same height
-                continue
-            elif z3 > z2 + 1:
-                # we're now higher up - no more bricks to check
-                break
-            elif z3 == z2 + 1:
+            if z3 == z2 + 1:
                 # is there overlap in x and y?
                 for x in range(x3, x4+1):
                     for y in range(y3, y4+1):
@@ -140,17 +143,22 @@ def part_1(lines):
             
     # now determine if safe to disintegrate
     def safe_to_disintegrate(brick):
+        # safe to disintegrate if all bricks it supports are supported by more than one brick
         for supported in supporting_list[brick]:
             if len(supported_by[supported]) <= 1:
                 return False
 
         return True
 
+    #supporting_list = [set(s) for s in supporting_list]
+    #supported_by = [set(s) for s in supported_by]
+
     num_to_disintegrate = 0
     for i in range(len(moved_bricks)):
-        #label = chr(ord('A') + i)  # generate label
-        #bricks = [chr(ord('A') + b) for b in supported_by[i]]
-        #print(f"{label}: {bricks}")
+        label = chr(ord('A') + i)  # generate label
+        bricks = [chr(ord('A') + b) for b in supporting_list[i]]
+        bricks2 = [chr(ord('A') + b) for b in supported_by[i]]
+        #print(f"{label}: supports {bricks}, supported_by {bricks2}")
         if safe_to_disintegrate(i):
             #print(f"Brick {label} is safe to disintegrate")
             num_to_disintegrate += 1
@@ -166,17 +174,32 @@ if __name__ == '__main__':
     with open('test.txt', 'r') as f:
         test_lines = f.readlines()
 
+    with open('test_2.txt', 'r') as f:
+        test_2_lines = f.readlines()
+
+    with open('test_3.txt', 'r') as f:
+        test_3_lines = f.readlines()
+
+    with open('test_4.txt', 'r') as f:
+        test_4_lines = f.readlines()
+
     with open('input.txt', 'r') as f:
         input_lines = f.readlines()
     
-    print("Part 1 ======================")
-    test_vals = part_1(test_lines)
-    print(f"Test output: {test_vals}")
+    #print("Part 1 ======================")
+    #test_vals = part_1(test_lines)
+    #print(f"Test output: {test_vals}")
+    #test_vals = part_1(test_2_lines)
+    #print(f"Test output: {test_vals}")
+    #test_vals = part_1(test_3_lines)
+    #print(f"Test output: {test_vals}")
+    #test_vals = part_1(test_4_lines)
+    #print(f"Test output: {test_vals}")
     input_vals = part_1(input_lines)
     print(f"Real output: {input_vals}")
 
-    print("Part 2 ======================")
-    test_vals = part_2(test_lines)
-    print(f"Test output: {test_vals}")
-    input_vals = part_2(input_lines)
-    print(f"Real output: {input_vals}")
+    # print("Part 2 ======================")
+    # test_vals = part_2(test_lines)
+    # print(f"Test output: {test_vals}")
+    # input_vals = part_2(input_lines)
+    # print(f"Real output: {input_vals}")
