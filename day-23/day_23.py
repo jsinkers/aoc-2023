084845b57a4c16afa435b69b25067a4cd6ed2457
James Sinclair <sinclair.j@gmail.com>
2024-01-30 17:56:15 +1100

    Day 23 in progress
    - working on test case

--- day-23/day_23.py
+++ day-23/day_23.py
@@ -88,7 +88,7 @@ def part_2(lines):
     # each node is a location 
     # each edge is weighted by path_length between each location
     # queue items are (prev node location, current location, current distance from prev node)
-    queue = [(start, (start[0], start[1]+1), 1)]
+    queue = [(start, (start[0], start[1]+1), 1, [start])]
     visited = set([start])
     # graph is a dict of nodes and their neighbours with the distance between them
     graph = {start: []}
@@ -96,23 +96,20 @@ def part_2(lines):
     # run bfs on map
     # every time a new node is found, add it to the graph
     while len(queue) > 0:
-        node1, current_pos, distance = queue.pop(0)
-        #print(f"Visited: {visited}")
+        node1, current_pos, distance, path = queue.pop(0)
+        #print(f"{node1} {current_pos} {distance} {path}")
         x, y = current_pos
-        if current_pos in visited:
-            continue
-
-        visited.add(current_pos)
 
         neighbours = []
         for nx, ny in get_neighbours(x, y):
-            if out_of_bounds(nx, ny) or grid[ny][nx] == '#' or (nx, ny) in visited:
+            if out_of_bounds(nx, ny) or grid[ny][nx] == '#' or (nx, ny) in path:
                 continue
         
             neighbours.append((nx, ny))
         
         # if there's more than 1 neighbour we have a new node
-        if len(neighbours) > 1:
+        if len(neighbours) > 1 or current_pos == target:
+            #print(current_pos)
             if node1 not in graph.keys():
                 graph[node1] = []
 
@@ -124,40 +121,38 @@ def part_2(lines):
         
         # now explore the neighbours
         for nx, ny in neighbours:
-            queue.append((node1, (nx, ny), distance))
+            queue.append((node1, (nx, ny), distance, path + [current_pos]))
 
     print(graph)
-    exit()
+    # now we have the reduced graph, determine the longest path
     queue = [(0, start, [])]
-    heapq.heapify(queue)
-    longest = 0
-    longest_path = []
-    slope_dx = {'^': (0, -1), '>': (1, 0), 'v': (0, 1), '<': (-1, 0)}
+    paths_to_end = []
 
     while len(queue) > 0:
         #print(f"Queue length: {len(queue)}")
-        priority, (x, y), path = heapq.heappop(queue)
-        print(f"priority: {priority}")
+        distance, node, path = queue.pop()
+        print(f"priority: {distance}")
 
+        print(f"{node}, path: {path}")
+        if node not in graph.keys():
+            print(f"Node not in graph {node}")
+            continue
 
-        #print(f"y: {y}, x: {x}, path: {path}")
-        for nx, ny in get_neighbours(x, y):
-            if out_of_bounds(nx, ny) or grid[ny][nx] == '#' or (nx, ny) in path:
+        for neighbour, step_dist in graph[node]:
+            if neighbour in path:
                 continue
                 
-            if (nx, ny) == target:
-                if len(path) + 1 > longest:
-                    longest = len(path) + 1
-                    longest_path = path.append((nx, ny))
-                    print(f"Longest path: {longest}")
-                    return longest
-            heapq.heappush(queue, (-len(path), (nx, ny), path + [(nx, ny)]))
+            new_path = path + [neighbour]
+            new_dist = distance + step_dist
+            if neighbour == target:
+                paths_to_end.append((new_path, new_dist))
+            else:
+                queue.append((new_dist, neighbour, new_path))
 
+    paths_to_end.sort(key=lambda x: x[1], reverse=True)
+    print(paths_to_end)
+    longest = paths_to_end[0][1]
     
-    for x, y in longest_path:
-        grid[y][x] = 'O'
-
-    print_grid(grid)
     return longest
     
 
@@ -177,5 +172,5 @@ if __name__ == '__main__':
     print("Part 2 ======================")
     test_vals = part_2(test_lines)
     print(f"Test output: {test_vals}")
-    input_vals = part_2(input_lines)
-    print(f"Real output: {input_vals}")
+    #input_vals = part_2(input_lines)
+    #print(f"Real output: {input_vals}")
